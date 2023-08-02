039a7d7d0c9b0c2ef55d38a71b3c3bf6a3ff080d
Gokul <88606340+gokulgk-9402@users.noreply.github.com>
2023-08-02 14:05:04 +0530

    Update config.js

--- firebase/config.js
+++ firebase/config.js
@@ -9,13 +9,7 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and lter, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAk8CYOa0pdIly3L_QnBOW7GhibMH--b7A",
-  authDomain: "expensetrackernextjs.firebaseapp.com",
-  projectId: "expensetrackernextjs",
-  storageBucket: "expensetrackernextjs.appspot.com",
-  messagingSenderId: "455399675729",
-  appId: "1:455399675729:web:60cfec8b8cace36b3a2570",
-  measurementId: "G-FD367VJ5VD",
+
 };
 
 // Initialize Firebase
