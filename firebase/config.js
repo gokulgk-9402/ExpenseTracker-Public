fe257880a60aedd7e9ec1445571cdb079ccaf18a
gokulgk-9402 <gokulgk9402@gmail.com>
2023-08-02 13:56:55 +0530

    Firebase credentials to environment variables

--- firebase/config.js
+++ firebase/config.js
@@ -3,19 +3,23 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { configDotenv } from "dotenv";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
+configDotenv();
+
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and lter, measurementId is optional
+console.log(process.env.FIREBASE_API_KEY);
 const firebaseConfig = {
-  apiKey: "AIzaSyAk8CYOa0pdIly3L_QnBOW7GhibMH--b7A",
-  authDomain: "expensetrackernextjs.firebaseapp.com",
-  projectId: "expensetrackernextjs",
-  storageBucket: "expensetrackernextjs.appspot.com",
-  messagingSenderId: "455399675729",
-  appId: "1:455399675729:web:60cfec8b8cace36b3a2570",
-  measurementId: "G-FD367VJ5VD",
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
